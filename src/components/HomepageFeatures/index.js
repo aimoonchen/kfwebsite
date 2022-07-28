986761d2a719b399e07d25c72d123713c89f7f8c
aimoonchen <kungfuchen@gmail.com>
2022-07-28 18:02:21 +0800

    daily commit

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// import Placeholder from 'react-bootstrap/Placeholder';
+// import Card from 'react-bootstrap/Card';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-
 const FeatureList = [
   {
     title: 'Easy to Use',
@@ -32,7 +36,6 @@ const FeatureList = [
     ),
   },
 ];
-
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
@@ -47,7 +50,7 @@ function Feature({Svg, title, description}) {
   );
 }
 
-export default function HomepageFeatures() {
+function KFEngineFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
@@ -58,5 +61,90 @@ export default function HomepageFeatures() {
         </div>
       </div>
     </section>
+  );
+}
+
+function ShowSandbox() {
+  return (
+    // <Card>
+    //   <Card.Body className="px-4 py-5 my-5 text-center">
+    //     <Card.Title><h2 className="display-4 fw-bold">Sandbox</h2></Card.Title>
+    //     <Card.Text>
+    //       This is a wider card with supporting text below as a natural lead-in
+    //       to additional content. This content is a little bit longer.
+    //     </Card.Text>
+    //   </Card.Body>
+    //   <Card.Img variant="bottom" src="/img/sandbox.png" alt="Sandbox image" />
+    // </Card>
+    <div className="px-4 py-5 my-5 text-center">
+      <h1 className="display-5 fw-bold">KFStudio</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">Quickly design and publish your game with KFStudio.</p>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+          <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        </div>
+        <br/>
+      </div>
+      <img className="shadow-lg d-block mx-auto mb-4 rounded" src="/img/sandbox.png"/>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <Container className="container marketing">
+      {/* <Row className="row">
+        <Col className="col-lg-4">
+          <Placeholder className="rounded-circle" style={{ width:"140px", height:"140px", background:"#777", color:"#777" }} />
+          <h2>Heading</h2>
+          <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+          <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+        </Col>
+        <Col className="col-lg-4">
+          <Placeholder className="rounded-circle" style={{ width:"140px", height:"140px", background:"#777", color:"#777" }} />
+          <h2>Heading</h2>
+          <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+          <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+        </Col>
+        <Col className="col-lg-4">
+          <Placeholder  className="rounded-circle" style={{ width:"140px", height:"140px", background:"#777", color:"#777" }} />
+          <h2>Heading</h2>
+          <p>And lastly this, the third column of representative placeholder content.</p>
+          <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+        </Col>
+      </Row> */}
+      <KFEngineFeatures/>
+      <ShowSandbox/>
+      <hr className="featurette-divider"/>
+      <Row className="row featurette">
+        <Col className="col-md-6 order-md-2">
+          <h2 className="featurette-heading">Effekseer</h2>
+          <p className="lead">Effekseer is a tool that allows easy creation of beautiful particle effects for games and movies.</p>
+          {/* <button type="button" className="btn btn-primary btn-lg px-4 gap-3">HomePage</button> */}
+          <a href="http://effekseer.github.io/en/" target="_blank">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">HomePage</button>
+          </a>
+        </Col>
+        <Col className="col-md-6 order-md-1">
+          <img className="shadow bd-placeholder-img-lg featurette-image img-fluid mx-auto rounded" src="/img/effekseer.png" style={{width:"640px", height:"360px", background:"#eee", color:"#aaa"}} />
+        </Col>
+      </Row>
+      <hr className="featurette-divider"/>
+      <Row className="row featurette">
+        <Col className="col-md-6">
+          <h2 className="featurette-heading">FairyGUI</h2>
+          <p className="lead">Professional game UI solution, Designer-friendly Editor, Support 10+ game engines, Help you double your productivity</p>
+          {/* <button type="button" className="btn btn-primary btn-lg px-4 gap-3">HomePage</button> */}
+          <a href="https://www.fairygui.com/" target="_blank">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">HomePage</button>
+          </a>
+        </Col>
+        <Col className="col-md-6">
+          <img className="shadow bd-placeholder-img-lg featurette-image img-fluid mx-auto rounded" src="/img/fairygui.png" style={{width:"618px", height:"320px", background:"#eee", color:"#aaa"}} />
+        </Col>
+      </Row>
+      <hr className="featurette-divider"/>
+    </Container>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,84 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+// import React from 'react';
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+// import clsx from 'clsx';
+// import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './carousel.css'
 
-import styles from './index.module.css';
+// import styles from './index.module.css';
+
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--secondary button--lg"
+//             to="/docs/intro">
+//             Get Started - 5min ⏱️
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Get Started - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/img/showcase/demo.png"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/img/showcase/mobagame.png"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/img/showcase/demo.png"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
